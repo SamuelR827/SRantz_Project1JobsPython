7703e1e84af8cd98d5118d71913d00f335ad8d6b
SamuelR827 <srrantz17@gmail.com>
2024-01-25 21:04:52 -0500

    add sample test

--- test_functions.py
+++ test_functions.py
@@ -1,15 +1,12 @@
 from io import StringIO
 
-import pytest
-
-from util_functions import write_page_to_file
+from util_functions import write_file_header
 
 
 def test_write_page_header():
     file = StringIO()
-    write_page_to_file({'mocked_results': 'data'}, 1, file)
+    write_file_header('Software Engineer', 'New York', file)
 
+    file.seek(0)
     content = file.read()
-    assert '"mocked_results": "data"' in content
-    assert '"Page 1"' in content
-    assert '-' * 256 in content
+    assert 'Generated output for Software Engineer in New York.' in content
